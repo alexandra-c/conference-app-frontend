72dc50d8d50aa024511b7796e3da85c9fde12409
Dragos Rosca <drosca@totalsoft.ro>
2021-09-30 13:02:48 +0300

    displayed data from GQL (atendees list)

--- src/apollo/defaultCacheData.js
+++ src/apollo/defaultCacheData.js
@@ -4,9 +4,8 @@ import { emailKey } from './cacheKeyFunctions'
 // Here you define the default values for local apollo state (@client only values)
 // https://www.apollographql.com/docs/react/local-state/local-state-management/
 
-const defaultEmail = { email: 'abc@gmail.com' }
+const defaultEmail = { email: 'admin@totalsoft.ro' }
 
 export const defaults = {
   [emailKey]: defaultEmail
 }
-
